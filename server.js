781bf2ddfea512c3906e7739531009158d9c61f4
Andreas Abaidoo <abaidooandy@gmail.com>
2022-10-02 16:23:39 +0000

    Added morgan middleware and serving static files

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-// set up express server
+// set up morgan middleware
+app.use(morgan('dev'));
+
+// set up express to serve files from the public folder
+app.use(express.static(__dirname + '/public'));
+
+// set up request and response of express server
 app.use((req, res) => {
-    console.log(req.headers);
     res.status = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
